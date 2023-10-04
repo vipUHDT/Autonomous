d1729ec8f6fefc3f492da8516c52650ac9b8a160
ElisDiep <116689861+ElisDiep@users.noreply.github.com>
2023-10-04 00:29:32 -1000

    Update mavlink.py

--- mavlink.py
+++ mavlink.py
@@ -12,13 +12,13 @@ waypoint_number = [1,2]  # You can increment this for each waypoint
 latitude_deg = [0,0]  # Desired latitude in degrees
 longitude_deg = [0,0]  # Desired longitude in degrees
 altitude_m = 60  # Desired altitude in meters above ground level (AGL)
-for x in range(2):
+for x in range(size(waypoint_number)):
     # Create a waypoint command
     waypoint_command = mavutil.mavlink.MAV_CMD_NAV_WAYPOINT
 
 
-
-    waypoint = [UAS.target_system,  #target_system
+    #parameter for waypoint
+    INT_SEND_WAYPOINT_parameter = [UAS.target_system,  #target_system
         UAS.target_component, #target_component
         0, #frame try setting to 3 for more accuracy
         waypoint_command, #MAV_CMD_NAV_WAYPOINT (16 ) or try to change it to  waypoint_command
@@ -30,16 +30,28 @@ for x in range(2):
         longitude_deg[x],   #autocontinue
     altitude_m]     #param1
     
+    #parameter for waypoint
+    LONG_SEND_WAYPOINT_parameter = [UAS.target_system,  #target_system
+        UAS.target_component, #target_component
+        0, #frame try setting to 3 for more accuracy
+        waypoint_command, #MAV_CMD_NAV_WAYPOINT (16) or try to change it to  waypoint_command
+        1, #confirmation 
+        0, #hold (s)
+        10, #Accept radius (m)
+        0, #pass radius (m)
+        0, #yaw (deg)
+        latitude_deg[x],  
+        longitude_deg[x],  
+    altitude_m]   
 
     # Send the waypoint command
+    UAS.mav.command_int_send(
+        *INT_SEND_WAYPOINT_parameter
+    )
+    
+    # Send the waypoint command
     UAS.mav.command_long_send(
-        UAS.target_system,
-        UAS.target_component,
-        waypoint_command,
-        1,  # Confirmation code (set to 0 for no confirmation)
-        waypoint_number,  # Waypoint number
-        *params  # Waypoint parameters 
-        #Note the '*' is used to unpack the elements in params list and pass the as seperate arguments to the command_long_send Method
+        *LONG_SEND_WAYPOINT_parameter
     )
 
     # Wait for confirmation (optional)
