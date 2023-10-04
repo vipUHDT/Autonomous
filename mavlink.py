de54b99bb00781502f7a8768249180c433b41478
ElisDiep <116689861+ElisDiep@users.noreply.github.com>
2023-10-04 12:00:17 -1000

    Update mavlink.py

--- mavlink.py
+++ mavlink.py
@@ -1,18 +1,22 @@
 from pymavlink import mavutil
 
+#https://mavlink.io/en/messages/common.html?q=#mav_commands
+#https://mavlink.io/en/messages/common.html?q=#COMMAND_LONG
+
 #UAS = mavutil.mavlink_connection('/dev/ttyACM0', baud=57600)
 UAS = mavutil.mavlink_connection('COM7', baud=57600)
-# Restart the ArduSub board !
+# Restart the Ardu board !
 #UAS.reboot_autopilot()
 #waiting for heartbeat before sending commands
-#UAS.wait_heartbeat()
+x=UAS.wait_heartbeat()
+print(x)
 
 # Define the waypoint parameters
 waypoint_number = [1,2]  # You can increment this for each waypoint
-latitude_deg = [0,0]  # Desired latitude in degrees
-longitude_deg = [0,0]  # Desired longitude in degrees
+latitude_deg = [21,21]  # Desired latitude in degrees
+longitude_deg = [57,57]  # Desired longitude in degrees
 altitude_m = 60  # Desired altitude in meters above ground level (AGL)
-for x in range(size(waypoint_number)):
+for x in range(2):
     # Create a waypoint command
     waypoint_command = mavutil.mavlink.MAV_CMD_NAV_WAYPOINT
 
@@ -22,6 +26,8 @@ for x in range(size(waypoint_number)):
         UAS.target_component, #target_component
         0, #frame try setting to 3 for more accuracy
         waypoint_command, #MAV_CMD_NAV_WAYPOINT (16 ) or try to change it to  waypoint_command
+        0, #current
+        0, #auto continue
         0, #param1 hold: wait # time at position (seconds)
         10, #param2 Accept radius (m)
         0, #param3 pass radius (m)
@@ -56,5 +62,6 @@ for x in range(size(waypoint_number)):
 
     # Wait for confirmation (optional)
     print("Waypoint sent. Waiting for confirmation...")
-    UAS.mav.wait_heartbeat()
+    x= UAS.wait_heartbeat()
+    print(x)
     print("Waypoint confirmed.")
