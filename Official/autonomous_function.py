5cbd1ba27db501d38fec3317b0bb7ccd3df6d2b0
ElisDiep <116689861+ElisDiep@users.noreply.github.com>
2023-11-03 01:57:59 -1000

    Update autonomous_function.py

--- Official/autonomous_function.py
+++ Official/autonomous_function.py
@@ -235,12 +235,12 @@ class CLASS:
         p2.start()
         p3.start()
         p4.start()
-        
+        '''
         p1.join()
         p2.join()
         p3.join()
         p4.join()
-
+        '''
         
         end = time.time()
         difference = end - start
